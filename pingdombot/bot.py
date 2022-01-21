2c6b70061ac8014d32c0ea4c3e52919036730bdb
Alex Kelly <alex.kelly@franklin.edu>
2022-01-21 16:51:48 -0500

    refactor: move to a main function

--- pingdombot/bot.py
+++ pingdombot/bot.py
@@ -4,23 +4,36 @@ from rich.console import Console
 from rich.table import Table
 import conf
 
-conf.load('conf.yaml')
 
-console = Console()
+def main():
+    # Load configuration, this will eventuall be handled by click
+    conf.load("conf.yaml")
 
-with console.status(f"Getting pingdom status...", spinner="point"):
-    response = requests.get(conf.BASE_URL + '/checks', auth=(conf.API_KEY, ''))
+    # initialize rich console
+    console = Console()
 
-table = Table(title='Pingdom Checks')
-table.add_column('Name', justify='left')
-table.add_column('Status', justify='center')
-table.add_column('Last Response', justify='center')
-for check in response.json()['checks']:
-    if check['status'] == 'up':
-        status = "[green]UP[/green]"
-    elif check['status'] == 'paused':
-        status = "[yellow]PAUSED[/yellow]"
-    else:
-        status = "[red]DOWN[/red]"
-    table.add_row(check['name'], status, str(check['lastresponsetime']))
-console.print(table)
+    # create a wait spinner while we get data from pingdom api
+    with console.status(f"Getting pingdom status...", spinner="point"):
+        response = requests.get(conf.BASE_URL + "/checks", auth=(conf.API_KEY, ""))
+
+    # create a rich table to display the data
+    table = Table(title="Pingdom Checks")
+    table.add_column("Name", justify="left")
+    table.add_column("Status", justify="center")
+    table.add_column("Last Response", justify="center")
+    # loop through all of the checks and add them to the table
+    for check in response.json()["checks"]:
+        # colorize the status based on the status code
+        if check["status"] == "up":
+            status = "[green]UP[/green]"
+        elif check["status"] == "paused":
+            status = "[yellow]PAUSED[/yellow]"
+        else:
+            status = "[red]DOWN[/red]"
+        table.add_row(check["name"], status, str(check["lastresponsetime"]))
+
+    console.print(table)
+
+
+if __name__ == "__main__":
+    main()
