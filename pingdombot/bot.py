8d9bdcdc54c4e1359d50ba25f248d5aa62752507
Alex Kelly <alex.kelly@franklin.edu>
2022-01-21 21:12:52 -0500

    feat: Add ability to choose statuses to display

--- pingdombot/bot.py
+++ pingdombot/bot.py
@@ -1,20 +1,36 @@
+"""main interface for the pingdombot"""
 import requests
-import rich
-from rich.console import Console
+import click
+import yaml
 from rich.table import Table
-import conf
+from rich.console import Console
 
+def get_config(configfile):
+    """get the config file"""
+    with open(configfile, 'r', encoding="utf-8")  as f:
+        config = yaml.safe_load(f)
+    return config
 
-def main():
-    # Load configuration, this will eventuall be handled by click
-    conf.load("conf.yaml")
+__version__ = "0.1.0"
+@click.command()
+@click.version_option(__version__, help='Show the version number and exit.')
+@click.option('--config', '-c', default='conf.yaml', type=click.Path(), help='config file to use')
+@click.option(
+    '--show', '-s',
+    default=['up','down', 'paused'],
+    multiple=True, help='The statuses to show'
+)
+def main(config,show):
+    """get the latest pingdom status"""
 
     # initialize rich console
     console = Console()
+    conf = get_config(config)
 
     # create a wait spinner while we get data from pingdom api
-    with console.status(f"Getting pingdom status...", spinner="point"):
-        response = requests.get(conf.BASE_URL + "/checks", auth=(conf.API_KEY, ""))
+    with console.status("Getting pingdom status...", spinner="point"):
+        response = requests.get(
+            conf["BASE_URL"] + "/checks", auth=(conf["API_KEY"], ""))
 
     # create a rich table to display the data
     table = Table(title="Pingdom Checks")
@@ -22,17 +38,23 @@ def main():
     table.add_column("Status", justify="center")
     table.add_column("Last Response", justify="center")
     # loop through all of the checks and add them to the table
+    count = 0
     for check in response.json()["checks"]:
-        # colorize the status based on the status code
-        if check["status"] == "up":
-            status = "[green]UP[/green]"
-        elif check["status"] == "paused":
-            status = "[yellow]PAUSED[/yellow]"
-        else:
-            status = "[red]DOWN[/red]"
-        table.add_row(check["name"], status, str(check["lastresponsetime"]))
+        if check["status"] == "up" and "up" in show:
+            count += 1
+            table.add_row(check["name"], "[green]UP[/green]", str(check["lastresponsetime"]))
+        elif check["status"] == "down" and "down" in show:
+            count += 1
+            table.add_row(check["name"], "[red]DOWN[/red]", str(check["lastresponsetime"]))
+        elif check["status"] == "paused" and "paused" in show:
+            count += 1
+            table.add_row(check["name"], "[yellow]Paused[/yellow]", str(check["lastresponsetime"]))
+#        table.add_row(check["name"], status, str(check["lastresponsetime"]))
 
-    console.print(table)
+    if not count == 0:
+        console.print(table)
+    else:
+        console.print("No hosts match your criteria")
 
 
 if __name__ == "__main__":
